497ff54d8b3ae4dd375c49e1b91bd3e850b93114
ZhukovKHV <zhukov.khv@yandex.ru>
2020-11-07 22:10:04 +1000

    1. Не выводит римские числа в ответ; 2. Не ограничивает ввод только римскими или арабскими числами;

--- src/com/zhukovkhv/Main.java
+++ src/com/zhukovkhv/Main.java
@@ -6,22 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
         Data in = new Data();
-        System.out.println(in.converOper());
+        int out = in.converOper();
+        System.out.println(out);
+        }
+
     }
-}
 
 class Data {
 
-    String arabarray[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
-    String rimarray[] = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
+    String[] arabarray = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    String[] rimarray = {"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"};
 
     Scanner enter = new Scanner(System.in);
     String equation = enter.nextLine();
-    String array[] = equation.split(" ");
+    String[] array = equation.split(" ");
 
-    int a;
+    int a, b , oper;
+
     public int convertDataA(){
-            for (int i = 0; i <= arabarray.length; i++) {
+        try {
+        for (int i = 0; i <= arabarray.length; i++) {
                 if (array[0].equals(arabarray[i])) {
                     a = i + 1;
                     return a;
@@ -30,12 +34,15 @@ class Data {
                     a = i + 1;
                     return a;
                 }
-            }
+        }}
+        catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Введите первое число от 1(I) до 10(X)");
+        }
     return a;
     }
 
-    int b;
     public int convertDataB(){
+        try{
         for (int i = 0; i <= arabarray.length; i++) {
             if (array[2].equals(arabarray[i])) {
                 b = i + 1;
@@ -45,24 +52,27 @@ class Data {
                 b = i + 1;
                 return b;
             }
+        }}
+        catch (ArrayIndexOutOfBoundsException ex){
+            System.out.println("Введите второе число от 1(I) до 10(X)");
         }
     return b;
     }
 
-    int oper;
     public int converOper(){
         a = convertDataA();
         b = convertDataB();
-        switch (array[1]){
-            case "+": oper = a + b; break;
-            case "-": oper = a - b; break;
-            case "*": oper = a * b; break;
-            case "/": oper = a / b; break;
-
-            default:
-                System.out.println("Недопустимое выражение");
-        return oper;
+        switch (array[1]) {
+            case "+" -> oper = a + b;
+            case "-" -> oper = a - b;
+            case "*" -> oper = a * b;
+            case "/" -> oper = a / b;
+            default -> System.out.println("Недопустимое выражение");
         }
     return oper;
     }
+}
+
+class convertArab {
+
 }
