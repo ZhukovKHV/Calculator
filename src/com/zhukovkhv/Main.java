ddc7fcb0c1546e1815490ab20320c0666840209d
ZhukovKHV <zhukov.khv@yandex.ru>
2020-11-09 22:36:35 +1000

    1. Выводит результаты в обоих числовых системах 2. Ввод возможен в обоих числовых системах

--- src/com/zhukovkhv/Main.java
+++ src/com/zhukovkhv/Main.java
@@ -6,11 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         Data in = new Data();
-        int out = in.converOper();
-        System.out.println(out);
-        }
+
+        System.out.println(in.convertOper());
+        System.out.println(in.convertRimOper());
 
     }
+}
 
 class Data {
 
@@ -21,7 +22,8 @@ class Data {
     String equation = enter.nextLine();
     String[] array = equation.split(" ");
 
-    int a, b , oper;
+    int a, b, oper;
+    String rimoper;
 
     public int convertDataA(){
         try {
@@ -49,7 +51,7 @@ class Data {
                 return b;
             } else
             if (array[2].equals(rimarray[i])) {
-                b = i + 1;
+                b =i + 1;
                 return b;
             }
         }}
@@ -59,7 +61,7 @@ class Data {
     return b;
     }
 
-    public int converOper(){
+    public int convertOper(){
         a = convertDataA();
         b = convertDataB();
         switch (array[1]) {
@@ -71,8 +73,132 @@ class Data {
         }
     return oper;
     }
+
+    public String convertRimOper(){
+        int out = convertOper();
+        String operstr = String.valueOf(out);
+        int remains = out % 10;
+        String remainsstr = String.valueOf(remains);
+
+       if (out >= 1 && out <= 10){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(operstr.equals(arabarray[j])){
+                    rimoper = rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 10 && out < 20){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "X" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 20 && out < 30){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "XX" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 30 && out < 40){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "XXX" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 40 && out < 50){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "XL" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 50 && out < 60){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "L" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 60 && out < 70){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "LX" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 70 && out < 80){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "LXX" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 80 && out < 90){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "LXXX" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if (out > 90 && out < 100){
+            for(int j = 0; j <= rimarray.length; j++){
+                if(remainsstr.equals(arabarray[j])){
+                    rimoper = "XC" + rimarray[j];
+                    return rimoper;
+                }
+            }
+        } else
+        if(operstr.equals("20")){
+            rimoper = "XX";
+            return rimoper;
+        } else
+        if(operstr.equals("30")){
+            rimoper = "XXX";
+            return rimoper;
+        }
+        switch (operstr) {
+            case "40" -> {
+                rimoper = "XL";
+                return rimoper;
+            }
+            case "50" -> {
+                rimoper = "L";
+                return rimoper;
+            }
+            case "60" -> {
+                rimoper = "LX";
+                return rimoper;
+            }
+            case "70" -> {
+                rimoper = "LXX";
+                return rimoper;
+            }
+            case "80" -> {
+                rimoper = "LXXX";
+                return rimoper;
+            }
+            case "90" -> {
+                rimoper = "XC";
+                return rimoper;
+            }
+            case "100" -> {
+                rimoper = "C";
+                return rimoper;
+            }
+        }
+    return rimoper;
+    }
 }
 
-class convertArab {
-
-}
